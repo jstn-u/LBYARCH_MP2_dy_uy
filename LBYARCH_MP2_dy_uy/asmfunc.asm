754661772c01a77510b029337e665d3e5ffcd1fc
jstn-u <justine_nicole_uy@dlsu.edu.ph>
2024-11-30 18:21:19 +0800

    testing 2

--- LBYARCH_MP2_dy_uy/asmfunc.asm
+++ LBYARCH_MP2_dy_uy/asmfunc.asm
@@ -1,5 +1,6 @@
 section .data
 msg db "Hello World",13,10,0
+msg1 db "try", 13, 10, 0
 
 section .text
 bits 64
@@ -10,7 +11,7 @@ extern printf
 
 asmhello:
 	sub rsp, 8*5 
-	lea rcx, [msg]
+	lea rcx, [msg1]
 	call printf
 	add rsp, 8*5
 	ret
