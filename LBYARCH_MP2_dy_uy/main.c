422007dab4aa5905992fa119e4bddf59b9c7420f
hccdyy <145318934+hccdyy@users.noreply.github.com>
2024-12-01 02:24:43 +0800

    average time

--- LBYARCH_MP2_dy_uy/main.c
+++ LBYARCH_MP2_dy_uy/main.c
@@ -6,6 +6,7 @@
 extern void imgCvtGrayFloatToInt(int height, int width, float* input, int* output);
 
 int main() {
+    long long int total = 0;
     int width, height;
     clock_t start, end;
     double time_taken;
@@ -21,7 +22,7 @@ int main() {
     // Check if memory allocation was successful
     if (input == NULL) {
         printf("Memory allocation failed.\n");
-        return 1; 
+        return 1;
     }
 
     // Input Float
@@ -30,31 +31,31 @@ int main() {
         scanf_s("%f", &input[i]);
     }
 
-    start = clock();
-    int i = 0;
-    for (i = 1; i <= 30; i++) {
+    for (int i = 1; i <= 30; i++) {
+        start = clock();
         imgCvtGrayFloatToInt(height, width, input, output);
-    }
-    end = clock();
-    int avg = ((double)(end - start) * 1000 / CLOCKS_PER_SEC)/i;
 
-    printf("Time taken %lf ms\n", avg);
-
-
-    // Display Integer
-    printf("Image (Float * 255) =\n");
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            printf("%d ", output[i * width + j]);
+        // Display Integer
+        printf("Image (Float * 255) =\n");
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                printf("%d ", output[i * width + j]);
+            }
+            printf("\n");
         }
-        printf("\n");
-    }
 
+
+        end = clock();
+        time_taken = (double)(end - start) * 1000 / CLOCKS_PER_SEC;
+        printf("Time taken: %lf ms\n", time_taken);
+        total += time_taken;
+    };
     free(input);
 
-    time_taken = (double)(end - start) * 1000 / CLOCKS_PER_SEC;
-    printf("Time taken: %lf ms\n", time_taken);
+    long long int avg = total / 30;
 
+    printf("Average for %dx%d: %d ms\n", width, height, avg);
 
     return 0;
 }
+
